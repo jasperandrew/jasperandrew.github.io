00757fd2a65ad66663cce7a7e714b7ae9217aafb
Jasper Andrew <the.quazillionaire@gmail.com>
2019-11-08 17:57:31 -0500

    namespace-ified shell

---
+++ js/shell-old.js
@@ -0,0 +1,187 @@
+'use strict';
+
+const shell = {
+    printing: false,
+    print_queue: [],
+    print_delay: true,
+
+    header: {
+        user: 'jasper',
+        pc: 'PC',
+
+        update() {
+            document.querySelector('#header').innerHTML = `${this.user}@${this.pc}:${sys.curr_dir.getPath()}`;
+        }
+    },
+
+    history: {
+        lvl: 0,
+        list: [],
+        curr: '',
+
+        nav(dir) {
+            let self = shell.history;
+            if(self.lvl === 0) self.curr = document.querySelector('#command').value;
+
+            if(dir === 'ArrowUp'){
+                self.lvl += (self.lvl > self.list.length-1 ? 0 : 1);
+            }else{
+                self.lvl += (self.lvl < 0 ? 0 : -1);
+            }
+        
+            if(self.lvl > 0) document.querySelector('#command').value = self.list[self.lvl-1];
+            else document.querySelector('#command').value = self.curr;		
+        },
+
+        add(cmd) {
+            let self = shell.history;
+            if(self.list[0] !== cmd) self.list.unshift(cmd);
+        }
+    },
+
+    cd(path) {
+        if(!path) path = '/home/jasper';
+        let file = sys.getFileFromPath(path, true);
+        if(!file){
+            shell.error(`${path}: does not exist`);
+            return false;
+        }
+        
+        if(file.type.search('fold') === -1){
+            shell.error(`${path}: not a directory`);
+            return false;
+        }
+        console.log(file);
+        sys.curr_dir = file;
+        // shell.header.update();
+        return true;
+    },
+
+    out(dest='>print') {
+
+    },
+
+    write(data='', path, append=false) {
+        const fp = new FilePath(path);
+        let file;
+        if(!fp.isValid()){
+            fp.up();
+            if(!fp.isValid()){
+                shell.error(`${fp.toString()} does not exist`);
+                return false;
+            }
+            file = new FSFile(fp.leaf, 'data', null);
+            fp.getFile().addFile(file);
+        }else{
+            file = fp.getFile();
+        }
+
+        if(file.type !== 'data'){
+            shell.error(`${file.getPath()} is not writable`);
+            return false;
+        }
+        
+        file.data = append ? file.data + data : data;
+        return true;
+    },
+
+    print(input='', newline=true) {
+        function doPrint() {
+            if(queue.length === 0){
+                if(shell.print_queue.length > 0){
+                    let p;
+                    [p, ...shell.print_queue] = shell.print_queue;
+                    if(util.typeof(p) === 'Array') queue = p;
+                    else queue = [p];
+                }else{
+                    shell.printing = false;
+                    return true;    
+                }
+            }
+
+            let out, split;
+            [out, ...queue] = queue;
+
+            if(out === null){
+                doPrint();
+                return true;
+            }
+            
+            out = out.toString();
+            if(out === undefined) out = '<<ERR>>';
+
+            split = out.split(/[\n\r]/);
+            if(out !== split[0]){
+                [out, ...split] = split;
+                for(let i = split.length-1; i >= 0; i--)
+                    queue.unshift(split[i]);
+            }
+
+            if(newline && out === '') out = ' '; // fix for visual glitch with <br>
+            window.setTimeout(() => {
+                doPrint();
+            }, shell.print_delay ? 17 : 0);
+            document.querySelector('#readout').innerHTML += out + (newline ? '\n' : '');
+        }
+
+        shell.print_queue.push(input);
+        if(shell.printing) return true;
+
+        let queue = [];
+        shell.printing = true;
+        doPrint();
+    },
+
+    error(msg) {
+        shell.print('[!] ' + msg);
+    },
+
+    run(argstr, dir=sys.getFileFromPath('/bin')) {
+        if(util.typeof(argstr) !== 'String'){
+            console.error('Arguments must be a string');
+            return false;
+        }
+
+        const args = util.parseArgs(argstr),
+              cmd = args[0];
+        console.log(args);
+
+        if(cmd === 'cd'){
+            shell.cd(args[1]);
+            return true;
+        }
+
+        if (dir.getData()[cmd] !== undefined){
+            const f = new Function(['args'], dir.getData()[cmd].getData());
+            return f(args);
+        }else{
+            shell.error(`${cmd}: command not found`);
+            return false;
+        }
+    },
+
+    submit(cmd) {
+        if(!cmd){
+            const prompt = document.querySelector('#command');
+            cmd = prompt.value;
+            prompt.value = '';
+        }
+
+        shell.print('> ' + cmd);
+
+        if(/\S/.test(cmd)){
+            shell.history.lvl = 0;
+            shell.history.add(cmd);
+            shell.run(cmd);
+        }
+    },
+
+    startup(delay=0) {
+        shell.run('clear');
+        window.setTimeout(() => {
+            const set = computer.settings.list;
+            if(set.welcome) shell.run('welcome');
+            if(set.cmd.length) set.cmd.forEach(c => shell.submit(c));	
+        }, delay);
+    }
+}
--- js/shell.js
+++ js/shell.js
@@ -1,186 +1,188 @@
+
 'use strict';
+(() => {
+    ////// Private ////////////////////
+    let user = 'jasper',
+        pc = 'PC',
+        printing = false,
+        print_queue = [],
+        print_delay = true,
 
-const shell = {
-    printing: false,
-    print_queue: [],
-    print_delay: true,
-
-    header: {
-        user: 'jasper',
-        pc: 'PC',
-
-        update() {
-            document.querySelector('#header').innerHTML = `${this.user}@${this.pc}:${sys.curr_dir.getPath()}`;
-        }
-    },
-
-    history: {
-        lvl: 0,
-        list: [],
-        curr: '',
-
-        nav(dir) {
-            let self = shell.history;
-            if(self.lvl === 0) self.curr = document.querySelector('#command').value;
-
-            if(dir === 'ArrowUp'){
-                self.lvl += (self.lvl > self.list.length-1 ? 0 : 1);
-            }else{
-                self.lvl += (self.lvl < 0 ? 0 : -1);
-            }
-        
-            if(self.lvl > 0) document.querySelector('#command').value = self.list[self.lvl-1];
-            else document.querySelector('#command').value = self.curr;		
+        error = (msg) => {
+            shell.print('[!] ' + msg);
+        },
+        updateHeader = () => {
+            document.querySelector('#header').innerHTML = `${user}@${pc}:${sys.curr_dir.getPath()}`;
         },
 
-        add(cmd) {
-            let self = shell.history;
-            if(self.list[0] !== cmd) self.list.unshift(cmd);
-        }
-    },
+    ////// Public /////////////////////
+        history = (() => {
+            let lvl = 0,
+                list = [],
+                curr = '',
+                
+                nav = (key) => {
+                    if(lvl === 0) curr = document.querySelector('#command').value;
 
-    cd(path) {
-        if(!path) path = '/home/jasper';
-        let file = sys.getFileFromPath(path, true);
-        if(!file){
-            shell.error(`${path}: does not exist`);
-            return false;
-        }
-        
-        if(file.type.search('fold') === -1){
-            shell.error(`${path}: not a directory`);
-            return false;
-        }
-        console.log(file);
-        sys.curr_dir = file;
-        // shell.header.update();
-        return true;
-    },
+                    if(key === 'ArrowUp'){
+                        lvl += (lvl > list.length-1 ? 0 : 1);
+                    }else if(key === 'ArrowDown'){
+                        lvl += (lvl < 0 ? 0 : -1);
+                    }
+                
+                    if(lvl > 0) document.querySelector('#command').value = list[lvl-1];
+                    else document.querySelector('#command').value = curr;    
+                },
+                
+                add = (cmd) => {
+                    if(list[0] !== cmd) list.unshift(cmd);
+                };
 
-    out(dest='>print') {
+            return { nav: nav, add: add };
+        })(),
 
-    },
-
-    write(data='', path, append=false) {
-        const fp = new FilePath(path);
-        let file;
-        if(!fp.isValid()){
-            fp.up();
-            if(!fp.isValid()){
-                shell.error(`${fp.toString()} does not exist`);
+        cd = (path) => {
+            if(!path) path = '/home/jasper';
+            let file = sys.getFileFromPath(path, true);
+            if(!file){
+                error(`${path}: does not exist`);
                 return false;
             }
-            file = new FSFile(fp.leaf, 'data', null);
-            fp.getFile().addFile(file);
-        }else{
-            file = fp.getFile();
-        }
-
-        if(file.type !== 'data'){
-            shell.error(`${file.getPath()} is not writable`);
-            return false;
-        }
-        
-        file.data = append ? file.data + data : data;
-        return true;
-    },
-
-    print(input='', newline=true) {
-        function doPrint() {
-            if(queue.length === 0){
-                if(shell.print_queue.length > 0){
-                    let p;
-                    [p, ...shell.print_queue] = shell.print_queue;
-                    if(util.typeof(p) === 'Array') queue = p;
-                    else queue = [p];
-                }else{
-                    shell.printing = false;
-                    return true;    
-                }
+            
+            if(file.type.search('fold') === -1){
+                error(`${path}: not a directory`);
+                return false;
             }
 
-            let out, split;
-            [out, ...queue] = queue;
-
-            if(out === null){
-                doPrint();
+            sys.curr_dir = file;
+            // header.update();
+            return true;    
+        },
+        print = (input='', newline=true) => {
+            function doPrint() {
+                if(queue.length === 0){
+                    if(print_queue.length > 0){
+                        let p;
+                        [p, ...print_queue] = print_queue;
+                        if(util.typeof(p) === 'Array') queue = p;
+                        else queue = [p];
+                    }else{
+                        printing = false;
+                        return true;    
+                    }
+                }
+    
+                let out, split;
+                [out, ...queue] = queue;
+    
+                if(out === null){
+                    doPrint();
+                    return true;
+                }
+                
+                out = out.toString();
+                if(out === undefined) out = '<<ERR>>';
+    
+                split = out.split(/[\n\r]/);
+                if(out !== split[0]){
+                    [out, ...split] = split;
+                    for(let i = split.length-1; i >= 0; i--)
+                        queue.unshift(split[i]);
+                }
+    
+                if(newline && out === '') out = ' '; // fix for visual glitch with <br>
+                window.setTimeout(() => {
+                    doPrint();
+                }, print_delay ? 17 : 0);
+                document.querySelector('#readout').innerHTML += out + (newline ? '\n' : '');
+            }
+    
+            print_queue.push(input);
+            if(printing) return true;
+    
+            let queue = [];
+            printing = true;
+            doPrint();
+        },
+        run = (argstr, dir=sys.getFileFromPath('/bin')) => {
+            if(util.typeof(argstr) !== 'String'){
+                console.error('Arguments must be a string');
+                return false;
+            }
+    
+            const args = util.parseArgs(argstr),
+                  cmd = args[0];
+            console.log(args);
+    
+            if(cmd === 'cd'){
+                cd(args[1]);
                 return true;
             }
-            
-            out = out.toString();
-            if(out === undefined) out = '<<ERR>>';
-
-            split = out.split(/[\n\r]/);
-            if(out !== split[0]){
-                [out, ...split] = split;
-                for(let i = split.length-1; i >= 0; i--)
-                    queue.unshift(split[i]);
+    
+            if (dir.getData()[cmd] !== undefined){
+                const f = new Function(['args'], dir.getData()[cmd].getData());
+                return f(args);
+            }else{
+                error(`${cmd}: command not found`);
+                return false;
             }
-
-            if(newline && out === '') out = ' '; // fix for visual glitch with <br>
+        },
+        startup = (delay=0) => {
+            run('clear');
             window.setTimeout(() => {
-                doPrint();
-            }, shell.print_delay ? 17 : 0);
-            document.querySelector('#readout').innerHTML += out + (newline ? '\n' : '');
-        }
-
-        shell.print_queue.push(input);
-        if(shell.printing) return true;
-
-        let queue = [];
-        shell.printing = true;
-        doPrint();
-    },
-
-    error(msg) {
-        shell.print('[!] ' + msg);
-    },
-
-    run(argstr, dir=sys.getFileFromPath('/bin')) {
-        if(util.typeof(argstr) !== 'String'){
-            console.error('Arguments must be a string');
-            return false;
-        }
-
-        const args = util.parseArgs(argstr),
-              cmd = args[0];
-
-        if(cmd === 'cd'){
-            shell.cd(args[1]);
+                const set = computer.settings.list;
+                if(set.welcome) run('welcome');
+                if(set.cmd.length) set.cmd.forEach(c => submit(c));	
+            }, delay);
+        },
+        submit = (cmd) => {
+            if(!cmd){
+                const prompt = document.querySelector('#command');
+                cmd = prompt.value;
+                prompt.value = '';
+            }
+    
+            print('> ' + cmd);
+    
+            if(/\S/.test(cmd)){
+                history.lvl = 0;
+                history.add(cmd);
+                run(cmd);
+            }
+        },
+        write = (data='', path, append=false) => {
+            const fp = new FilePath(path);
+            let file;
+            if(!fp.isValid()){
+                fp.up();
+                if(!fp.isValid()){
+                    error(`${fp.toString()} does not exist`);
+                    return false;
+                }
+                file = new FSFile(fp.leaf, 'data', null);
+                fp.getFile().addFile(file);
+            }else{
+                file = fp.getFile();
+            }
+    
+            if(file.type !== 'data'){
+                error(`${file.getPath()} is not writable`);
+                return false;
+            }
+            
+            file.data = append ? file.data + data : data;
             return true;
-        }
+        };
+    
+    ////// Output /////////////////////
+    this.shell = {
+        history: history,
 
-        if (dir.getData()[cmd] !== undefined){
-            const f = new Function(['args'], dir.getData()[cmd].getData());
-            return f(args);
-        }else{
-            shell.error(`${cmd}: command not found`);
-            return false;
-        }
-    },
-
-    submit(cmd) {
-        if(!cmd){
-            const prompt = document.querySelector('#command');
-            cmd = prompt.value;
-            prompt.value = '';
-        }
-
-        shell.print('> ' + cmd);
-
-        if(/\S/.test(cmd)){
-            shell.history.lvl = 0;
-            shell.history.add(cmd);
-            shell.run(cmd);
-        }
-    },
-
-    startup(delay=0) {
-        shell.run('clear');
-        window.setTimeout(() => {
-            const set = computer.settings.list;
-            if(set.welcome) shell.run('welcome');
-            if(set.cmd.length) set.cmd.forEach(c => shell.submit(c));	
-        }, delay);
-    }
-}
+        cd: cd,
+        print: print,
+        run: run,
+        startup: startup,
+        submit: submit,
+        write: write,
+    };
+})();
